6fcdf39cc7ce2449f44dc7274e3850e4114e6c36
Lance Fallon <fallon.lance@gmail.com>
2018-04-19 17:09:15 -0400

    ldap

--- src/main/java/com/sinclair/vpreports/spreadsheetrefresh/config/security/service/ADConstants.java
+++ src/main/java/com/sinclair/vpreports/spreadsheetrefresh/config/security/service/ADConstants.java
@@ -2,19 +2,19 @@ package com.sinclair.vpreports.spreadsheetrefresh.config.security.service;
 
 public interface ADConstants {
 
-    String INITIAL_CONTEXT_FACTORY = "";
+    String INITIAL_CONTEXT_FACTORY = "com.sun.jndi.ldap.LdapCtxFactory";
 
-    String PROVIDER_URL = "";
+    String PROVIDER_URL = "ldap://10.99.50.5:389";
 
-    String SIMPLE_SECURITY_AUTHENTICATION = "";
+    String SIMPLE_SECURITY_AUTHENTICATION = "simple";
 
-    String SERVER_SECURITY_PRINCIPAL = "";
+    String SERVER_SECURITY_PRINCIPAL = "CN=Corporate Service Account svc_palasauth,OU=ServiceAccounts,DC=sbgnet,DC=int";
 
-    String SERVER_SECURITY_CREDENTIALS = "";
+    String SERVER_SECURITY_CREDENTIALS = "Pa1a5aut41!";
 
-    String SERVER_BASEDN = "";
+    String SERVER_BASEDN = "DC=sbgnet,DC=int";
 
-    String MEMBER_OF = "";
+    String MEMBER_OF = "memberOf";
 
-    String CN = "";
+    String CN = "cn";
 }
